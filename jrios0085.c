5f4bab77ea62b009bdc23f85718d39a368952dcd
Jrios0085 <Kappatron53@gmail.com>
2024-05-15 17:35:15 -0700

    Create jrios0085.c

---
+++ jrios0085.c
@@ -0,0 +1,71 @@
+//contributors
+//Jonathan rios
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <math.h>
+
+
+ void doorChoiceRoom101 ()
+
+int you_are_in_a_swamp_side_of_the_dungeon();
+void func101()
+
+void exploreRoom(int doorChoiceRoom101);
+
+void doorChoiceRoom101
+while(choice != 8)
+{
+puts("you are in the swampy side of thhis convoluted dungeon")
+puts("nonetheless you bare your wits even without mana as you are the one true wizard")
+puts("the room is wide but smelly and is filled with corpses but")
+puts("it is also filled with traps, portals to other rooms and loot")
+puts("you have 8 choices think wisely on what you want to do")
+
+scanf("%d",&choice);
+
+switch(choice)
+{
+case 1:
+{
+
+puts("doorChoiceRoom101");
+puts("you walk forward to the swamp of room101");
+while (getchar() != '\n');
+
+printf("\nWhile you were walking forward to the swamp your eyes locked onto something\n", );
+while ((getchar() != '\n'););
+
+printf("\n what your eyes locked to was nothing but a corpse of a dead beaurcrat\n");
+while (getchar() != '\n');
+
+printf("\n you think to yourself: man what a terrible night to be in a dungeon with a dead beaurcrat\n")
+while (getchar() != '\n');
+
+printf("\nbut after some thought you see a shiny item near the beaurcrat's hand\n")
+while (getchar() != '\n');
+
+printf("\nwhat luck you found a shiny coin\n");
+while (getchar() != '\n');
+
+printf("\nEnding 1\n"); 
+while (getchar() != '\n');
+ break;
+
+}
+ case 2:
+{
+
+ 	puts("\nYou decided to stay put and wait for someone to come for you\n");
+while (getchar() != '\n');
+
+printf("\nyou were so bored of waiting that you fell asleep\n"); 
+while (getchar() != '\n');
+
+printf("\n you died in your sleep as some monsters killed you while you were sleeping\n");
+while (getchar() != '\n');
+break;
