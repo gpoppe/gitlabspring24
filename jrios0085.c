135f7026981b9e86f5741d4fddf8f75ce65466e9
Jrios0085 <Kappatron53@gmail.com>
2024-05-16 12:08:21 -0700

    update room 8
    updated room 8 as well as case 1.

--- jrios0085.c
+++ jrios0085.c
@@ -10,49 +10,115 @@
 #include <math.h>
 
 
- void doorChoiceRoom101 ()
-//
-int you_are_in_a_swamp_side_of_the_dungeon();
-void func101()
-
-void exploreRoom(int doorChoiceRoom101);
-
-void doorChoiceRoom101
-while(choice != 8)
+ case 8:
 {
-puts("you are in the swampy side of thhis convoluted dungeon")
-puts("nonetheless you bare your wits even without mana as you are the one true wizard")
-puts("the room is wide but smelly and is filled with corpses but")
+puts("room8");
+	int choice = 0;
+				char playerName[6];
+				srand(time(NULL));
+				    
+				printf("Please enter your player name: ");
+				scanf("%s", playerName);
+				
+				printf("welcome traveler, you are in the Swamp side of the kingdom"
+					while(getchar() != '\n');
+
+				printf("it is met with very ferocious monsters, wild animals and worse of all, crocodiles")	
+				while(getchar() != '\n');
+				
+				printf("\nTraveler %s, in order to progress the story  you must use the ENTER key during gameplay.\n", playerName);
+				
+				doorChoiceRoom8();
+while(choice != 7)
+{
+puts("\nyou are in the swampy side of the kingdom")
+puts("nonetheless you bare your wits since you are the one true traveler")
+puts("that side of castle is wide but smelly and is filled with corpses but")
 puts("it is also filled with traps, portals to other rooms and loot")
 puts("you have 8 choices think wisely on what you want to do")
-
+puts("|1: go straight,|2: stay put, because why not|3: go left,|4: go back to the path you were in before, |5: go right,| 6: exit the game in case you are tired of this adventure| 7: 
+secret route  )
 scanf("%d",&choice);
 
 switch(choice)
 {
 case 1:
 {
-
-puts("doorChoiceRoom101");
-puts("you walk forward to the swamp of room101");
+puts("\n|you went forward|\n");
 while (getchar() != '\n');
 
-printf("\nWhile you were walking forward to the swamp your eyes locked onto something\n", );
+printf("you walk forward to the swamp");
+while (getchar() != '\n');
+
+printf("While you were walking forward to the swamp your eyes locked onto something");
 while ((getchar() != '\n'););
 
-printf("\n what your eyes locked to was nothing but a corpse of a dead beaurcrat\n");
+printf(" what your eyes locked to was nothing but a corpse of a dead beaurcrat");
 while (getchar() != '\n');
 
-printf("\n you think to yourself: man what a terrible night to be in a dungeon with a dead beaurcrat\n")
+printf("you think to yourself: man what a terrible night to be in a dungeon with a dead beaurcrat")
 while (getchar() != '\n');
 
-printf("\nbut after some thought you see a shiny item near the beaurcrat's hand\n")
+printf("but after some thought you see a shiny item near the beaurcrat's hand")
 while (getchar() != '\n');
 
-printf("\nwhat luck you found a shiny coin\n");
+printf("what luck you found a shiny coin");
 while (getchar() != '\n');
 
-printf("\nEnding 1\n"); 
+printf("now you must flip a coin to see if there is a status affect from getting the shiny coin from that corpse")
+while (getchar() != '\n');
+
+void ajlSpace()
+{
+    while (getchar() != '\n')
+        ;
+}
+
+void coinFlip() {
+  int call = 0;
+
+  printf("You know the name of the game : call heads{1} or tails{2}: ");
+  scanf("%d", &call);
+    
+  if (call == 1 || call == 2) {
+    printf("The coin flips!\n");
+    
+  }
+  else 
+  {
+    printf("You call an invalid number, so your opponent calls instead!\n");
+    call = (rand() % 2) + 1;
+    
+    if (call == 1){
+      printf("Luck was not on your side Fellow Traveler!\n");
+    }
+    else
+     {
+      printf("You got yourself a rare relic\n");
+    }
+  }
+
+    int coin = (rand() % 2) + 1;
+
+  if (coin == 1 && call == 1) {
+    printf("It's heads, what luck, you found something valuable!\n");
+  }
+  else if (coin == 1 && call == 2) {
+    printf("It's heads.,what you got from the beaurcrat is the coin of Bulk Bogan.\n");
+  }
+  else if (coin == 2 && call == 1) {
+    printf("It's tails. well can't win them all, Too bad!\n");
+  }
+  else if (coin == 2 && call == 2) {
+    printf("It's tails!, well you were cursed from touching the coin of the beaurcrat!\n");
+  }
+  
+  printf("you did not pass the flip test"); 
+while (getchar() != '\n');
+
+printf("you were cursed by touching the coin, what curse you may ask? well its a curse that turns anyone who fails the coin flip test into a corpse"); 
+while (getchar() != '\n');
+printf("you died, end of story, Ending 1"); 
 while (getchar() != '\n');
  break;
 
@@ -63,10 +129,10 @@ while (getchar() != '\n');
  	puts("\nYou decided to stay put and wait for someone to come for you\n");
 while (getchar() != '\n');
 
-printf("\nyou were so bored of waiting that you fell asleep\n"); 
+printf("you were so bored of waiting that you fell asleep"); 
 while (getchar() != '\n');
 
-printf("\n you died in your sleep as some monsters killed you while you were sleeping\n");
+printf(" you died in your sleep as some monsters killed you while you were sleeping");
 while (getchar() != '\n');
 break;
 
